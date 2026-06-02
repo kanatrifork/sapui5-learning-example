4882fa7aaf8110089b714b5dd9d7731589c65a73
kanatrifork <kana@trifork.com>
2026-06-02 10:14:59 +0200

    xml fragment exercise

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,14 +1,35 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/syncStyleClass"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, syncStyleClass) {
+    "use strict";
 
-        return Controller.extend("sap.training.exc.controller.Overview", {
-
-
+    return Controller.extend("sap.training.exc.controller.Overview", {
+      onSave: function () {
+        if (!this.pDialog) {
+          this.pDialog = this.loadFragment({
+            name: "sap.training.exc.view.Dialog",
+          }).then(
+            function (oDialog) {
+              syncStyleClass(
+                this.getOwnerComponent().getContentDensityClass(),
+                this.getView(),
+                oDialog,
+              );
+              return oDialog;
+            }.bind(this),
+          );
+        }
+        this.pDialog.then(function (oDialog) {
+          oDialog.open();
         });
+      },
+
+      onCloseDialog: function () {
+        this.byId("dialog").close();
+      },
     });
+  },
+);
